42e94a5d5f9b41344e812146e68f6601f601e76d
faqih <faqih.syakir11@gmail.com>
2025-01-10 21:38:58 +0700

    test view

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -11,6 +11,8 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+    <link rel="stylesheet" href="{{ asset('/resources/css/app.css') }}">
+
     <!-- Styles / Scripts -->
     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
         @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -1171,7 +1173,9 @@
 
 </body>
 
+    <script src="{{ asset('/resources/js/app.js') }}"></script>
     <script>
+
         function handleTujuanChange(select) {
             const lainnyaContainer = document.getElementById('tujuanLainnyaContainer');
             const tujuanLainnyaInput = document.getElementById('tujuan_lainnya');
